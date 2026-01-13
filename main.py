2c2977c45fd9d0c132770c7c0a85145ad43be3cb
Nathan <158613820+bankruptcies@users.noreply.github.com>
2026-01-13 14:31:44 +0000

    Added favorite movie

--- main.py
+++ main.py
@@ -41,3 +41,7 @@ if __name__ == "__main__":
     movie = get_movie("Inception")
     print(movie)
     print(movie["title"])
+
+    movie = get_movie("Top Gun")
+    print(movie)
+    print(movie["title"])
